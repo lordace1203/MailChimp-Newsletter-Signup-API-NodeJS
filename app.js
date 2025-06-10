fcf49f43d1592751d6cc9284c6e5e22312513ab7
Moshood Abdulwaheed <97702101+lordace1203@users.noreply.github.com>
2025-06-10 14:25:53 +0100

    First Commit

--- app.js
+++ app.js
@@ -2,29 +2,33 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const request = require("request");
+require('dotenv').config();
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.listen(process.env.POST || 3000, function (){
-    console.log("Server is Running: 3000");
+app.listen(3000, function () {
+    console.log("Server Running:3000");
 })
 
-
-app.get("/", function (req, res){
-    res.sendFile(__dirname+"/signup.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
 })
 
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+})
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
+
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
 
     console.log(firstName, lastName, email);
-    
+
     const data = {
         members: [{
             email_address: email,
@@ -35,42 +39,30 @@ app.post("/", function(req, res){
             }
         }]
     }
+    const jsonData = JSON.stringify(data);
 
-const jsonData = JSON.stringify(data);
-
-const url = "https:us9.api.mailchimp.com/3.0/lists/cc86fc34e1"
-const options = {
-    method:"post",
-    auth: "ace:8e0ee814b82032634730fd3f1a08b014-us9"
-}
-
-
-const request = https.request(url, options, function (response){
-response.on("data", function (data){
-    if (response.statusCode === 200) {
-        res.sendFile(__dirname+"/success.html");
-    } else {
-        res.sendFile(__dirname+"/failure.html");
+    const apiKey = process.env.apiKey;
+    const audienceId = process.env.audienceId;
+    const url = `https://us9.api.mailchimp.com/3.0/lists/${audienceId}`
+    const options = {
+        method: "post",
+        auth: `ace:${apiKey}`
     }
-})
-})
-request.write(jsonData);
-request.end();
+
+    const request = https.request(url, options, function (response) {
+        response.on("data", function (data) {
+            if (response.statusCode === 200) {
+                res.sendFile(__dirname + "/success.html");
+            } else {
+                res.sendFile(__dirname + "/failure.html");
+            }
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
 
 })
-
-
-app.post("/failure", function(req, res){
-    res.redirect("/");
-})
-
-
-// API KEY
-// 8e0ee814b82032634730fd3f1a08b014-us9
-
-
-// Audience ID
-// cc86fc34e1
 
 
 // endpoint
